91df68a56d19301b31d23e2de57f021d08116f98
thiampo <babacathiam@gmail.com>
2021-05-27 01:56:33 +0000

    projet

---
+++ ServeurApp/apiRouter.js
@@ -0,0 +1,24 @@
+
+// Imports
+var express      = require('express');
+var usersCtrl    = require('./routes/usersCtrl');
+
+
+
+// Router
+exports.router = (function() {
+  var apiRouter = express.Router();
+
+  // Users routes
+ apiRouter.route('/users/register/').post(usersCtrl.register);
+  apiRouter.route('/users/login/').post(usersCtrl.login);
+  apiRouter.route('/users/me/').post(usersCtrl.getUserProfile);
+ // apiRouter.route('/users/me/').put(usersCtrl.updateUserProfile);
+  apiRouter.route('/users/all').get(usersCtrl.list);
+  apiRouter.route('/users/supprimer').post(usersCtrl.supp);
+ 
+ // apiRouter.route('/users/modifier/').post(usersCtrl.modifier);
+  //apiRouter.route('/users/detail/').post(usersCtrl.detail);
+
+  return apiRouter;
+})();
---
+++ frontend/src/app/modification/modification.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { LoginService } from './../login.service';
+@Component({
+  selector: 'app-modification',
+  templateUrl: './modification.component.html',
+  styleUrls: ['./modification.component.css']
+})
+export class ModificationComponent implements OnInit {
+  id:any
+  email:any
+  dataUser : any
+  nomUser:any
+  prenomUser:any
+  roleUser:any
+  telUser:any
+  constructor(private LoginService:LoginService, private router:Router,private route:ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.id=this.route.snapshot.params['id']
+  this.id={value:this.id}
+  console.log(this.id)
+  this.LoginService.recevoir(this.id).subscribe((resultat)=>{
+  this.dataUser=resultat
+  this.nomUser=this.dataUser.nom
+  this.prenomUser=this.dataUser.prenom
+  this.roleUser=this.dataUser.role
+  this.telUser=this.dataUser.tel
+  console.log(resultat)
+
+  })
+
+
+  }
+  connecter(f: { value: object; }){
+    console.log("hello")
+    console.log(f.value)
+    return this.LoginService.donne(f.value).subscribe((resultat:any)=>{
+      console.log(resultat)
+      //this.router.navigate(['admin/'+this.id])
+  })
+   }
+
+}
